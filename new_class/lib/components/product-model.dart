775fd1bc7b360e87d5e88fd739f5a8a6dbc312a6
Saad Kashif <115474924+code-with-saad@users.noreply.github.com>
2023-01-08 17:17:47 +0500

    Update product-model.dart

--- new_class/lib/components/product-model.dart
+++ new_class/lib/components/product-model.dart
@@ -1,24 +1,24 @@
 List<Map> products = [
-  {"id": 1, "name": "Airpods", "price": 3000, "isfavourite": false},
-  {"id": 2, "name": "Iphone", "price": 70000, "isfavourite": false},
-  {"id": 3, "name": "CPU", "price": 50000, "isfavourite": false},
+  {"id": 1, "name": "Airpods", "price": 3000, "isfavourite": false, "iscart": false},
+  {"id": 2, "name": "Iphone", "price": 70000, "isfavourite": false, "iscart": false},
+  {"id": 3, "name": "CPU", "price": 50000, "isfavourite": false, "iscart": false},
 
-  {"id": 4, "name": "Mouse", "price": 500, "isfavourite": false},
+  {"id": 4, "name": "Mouse", "price": 500, "isfavourite": false, "iscart": false},
 
-  {"id": 5, "name": "Keyboard", "price": 1000, "isfavourite": false},
+  {"id": 5, "name": "Keyboard", "price": 1000, "isfavourite": false, "iscart": false},
 
-  {"id": 6, "name": "Laptop", "price": 60000, "isfavourite": false},
-  {"id": 7, "name": "Headphone", "price": 2000, "isfavourite": false},
-  {"id": 8, "name": "Ram", "price": 800, "isfavourite": false},
+  {"id": 6, "name": "Laptop", "price": 60000, "isfavourite": false, "iscart": false},
+  {"id": 7, "name": "Headphone", "price": 2000, "isfavourite": false, "iscart": false},
+  {"id": 8, "name": "Ram", "price": 800, "isfavourite": false, "iscart": false},
 
-  {"id": 9, "name": "Hardisk", "price": 1500, "isfavourite": false},
-  {"id": 10, "name": "Earphone", "price": 500, "isfavourite": false},
+  {"id": 9, "name": "Hardisk", "price": 1500, "isfavourite": false, "iscart": false},
+  {"id": 10, "name": "Earphone", "price": 500, "isfavourite": false, "iscart": false},
   
-  {"id": 11, "name": "Samsung", "price": 35000, "isfavourite": false},
-  {"id": 12, "name": "Realme", "price": 30000, "isfavourite": false},
-  {"id": 13, "name": "Infinix", "price": 20000, "isfavourite": false},
-  {"id": 14, "name": "Led Screen", "price": 2000, "isfavourite": false},
-  {"id": 15, "name": "USB", "price": 1500, "isfavourite": false},
+  {"id": 11, "name": "Samsung", "price": 35000, "isfavourite": false, "iscart": false},
+  {"id": 12, "name": "Realme", "price": 30000, "isfavourite": false, "iscart": false},
+  {"id": 13, "name": "Infinix", "price": 20000, "isfavourite": false, "iscart": false},
+  {"id": 14, "name": "Led Screen", "price": 2000, "isfavourite": false, "iscart": false},
+  {"id": 15, "name": "USB", "price": 1500, "isfavourite": false, "iscart": false},
 ];
 
 List<Map> favourite = [];
